8c79931b3e76f6ae8511886f8c961c88bf41169b
robertmirro <rmirro@gmail.com>
2014-06-02 17:33:31 -0400

    inti commit version 3, add jquery dependency to main_3.js...

---
+++ app_3.js
@@ -0,0 +1,19 @@
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+    baseUrl: 'js/lib',
+    paths: {
+        app: '../app',
+        jquery: 'jquery-2.1.1.min'
+    }
+});
+
+console.log('inside app_3.js...');
+
+// Start loading the main app file. Put all of
+// your application logic in there.
+requirejs(['app/main_3'], function(main3) {
+	console.log('inside app_3.js: main3.message: %s\n%O', main3.message, main3);
+	}
+);
---
+++ js/app/main_3.js
@@ -0,0 +1,17 @@
+define(["jquery"], function ($) {
+    // Load any app-specific modules
+    // with a relative require call,
+    // like:
+    //var messages = require('./messages');
+
+    // Load library/vendor modules using
+    // full IDs, like:
+    //var print = require('print');
+
+    //print(messages.getHello());
+
+    console.log('inside main_3.js...\njQuery:\n%O', $);
+    $('#message').html('<h4>Message returned via jQuery from main_3.js...</h4>');
+
+    return {name: "main_3.js", type: "module data", message: "data returned from main_3.js..."};
+});
