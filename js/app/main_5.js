05438171f3291fe38ebec38856353df03bb3af4f
robertmirro <rmirro@gmail.com>
2014-06-02 19:17:25 -0400

    add some underscore logic

--- js/app/main_5.js
+++ js/app/main_5.js
@@ -10,11 +10,13 @@ define(['jquery', 'underscore'], function ( $ , _ ) {
 
     console.log('inside main_5.js...\nUnderscore:\n%O', _);
 
-
     $(function () {
-		$('#message').html('<h4>Message returned via jQuery from main_5.js on domready event...</h4>');
-	});
+		var libs = [{name : 'RequireJS', libNum : '1'}, {name : 'jQuery', libNum : '2'}, {name : 'Underscore', libNum : '3'}, {name : '&lt;&lt;next library goes here&gt;&gt;', libNum : '4'}];
+		var names = _(libs).pluck('name').map(function (value){return ' ' + value + '.js'});
+		console.log('Libraries in use: %s', names.toString());
 
+		$('#message').html('<h4>Message returned via jQuery from main_5.js on domready event...</h4>').append('<b>Libraries in use:</b> ' + names.toString());
+	});
 
     return {
 		name: "main_5.js",
