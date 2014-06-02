f700a9c82eabb8953075bfc4342e02762ab69b4b
robertmirro <rmirro@gmail.com>
2014-06-02 18:26:42 -0400

    cleanup code for version 4

--- js/app/main_4.js
+++ js/app/main_4.js
@@ -1,13 +1,4 @@
 define(function (require) {
-    // Load any app-specific modules
-    // with a relative require call,
-    // like:
-    //var messages = require('./messages');
-
-    // Load library/vendor modules using
-    // full IDs, like:
-    //var print = require('print');
-
 
 	console.log("require: \n%O", require);
 
